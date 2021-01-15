0a4c5c73a64631552526a241464ed44a6c154b01
gfrancoo <gianfranco.aguirre.cruz@gmail.com>
2021-01-15 01:19:35 -0500

    Create Home Component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Track } from '../model/entities/track';
+import { ChartService } from '../model/services/chart.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  chartTracks: Track[];
+  constructor(
+    private chartService: ChartService
+  ) { }
+
+  ngOnInit() {
+    this.chartService.get().subscribe(
+      data => {
+        this.chartTracks = data.tracks.data
+      }
+    )
+  }
+
+}
