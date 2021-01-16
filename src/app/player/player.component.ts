cb98d5d129edc1cf250ce1bb9a9c79316d57e2b7
gfrancoo <gianfranco.aguirre.cruz@gmail.com>
2021-01-16 01:33:56 -0500

    Change player design

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from '../model/entities/album';
+import { Artist } from '../model/entities/artist';
+import { Track } from '../model/entities/track';
 
 @Component({
   selector: 'app-player',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerComponent implements OnInit {
 
+  track: Track;
   constructor() { }
 
   ngOnInit() {
+    this.track = new Track();
+    this.track.title = 'Una vez';
+    this.track.artist = new Artist();
+    this.track.artist.name = 'Bad Bunny';
+    this.track.album = new Album();
+    this.track.album.title = 'YHLQMDLG';
+    this.track.album.cover_medium = 'https://musica.news/wp-content/uploads/2020/03/Portada-de-YHLQMDLG-Bud-Bunny.jpg';
   }
 
 }
